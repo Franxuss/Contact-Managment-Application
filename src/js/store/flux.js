c18b7ba519617bd368a57a43137247a3654c0fa2
Tibisfly <vielmatorotibisay@gmail.com>
2020-10-30 20:20:52 +0000

    contact managment new

--- src/js/store/flux.js
+++ src/js/store/flux.js
@@ -1,13 +1,119 @@
+// const agendaSlug = "Tibisfly";
 const getState = ({ getStore, setStore }) => {
 	return {
 		store: {
 			//Your data structures, A.K.A Entities
+			contacts: []
 		},
 		actions: {
-			//(Arrow) Functions that update the Store
-			// Remember to use the scope: scope.state.store & scope.setState()
+			// createContact: async data => {
+			// 	let endpoint = await fetch("https://assets.breatheco.de/apis/fake/contact/Tibisay", {
+			// 		method: "POST",
+			// 		mode: "cors",
+			// 		redirect: "follow",
+			// 		headers: new Headers({
+			// 			"Content-Type": "application/json"
+			// 		}),
+			// 		body: JSON.stringify({
+			// 			full_name: data.name,
+			// 			email: data.email,
+			// 			agenda_slug: "Tibisay",
+			// 			address: data.address,
+			// 			phone: data.phone
+			// 		})
+			// 	});
+			// 	endpoint = await endpoint.json();
+			// },
+			listContacts(slug) {
+				const store = setStore();
+				const endpoint = "https://assets.breatheco.de/apis/fake/contact/agenda/" + slug;
+				const config = {
+					method: "GET"
+				};
+				fetch(endpoint, config)
+					.then(response => {
+						return response.json();
+					})
+					.then(json => {
+						setStore({ contacts: json });
+					});
+			},
+			createContact(data) {
+				const endpoint = "https://assets.breatheco.de/apis/fake/contact/";
+				const config = {
+					method: "POST",
+					body: JSON.stringify({
+						full_name: data.name,
+						email: data.email,
+						agenda_slug: "Tibisfly",
+						address: data.address,
+						phone: data.phone
+					}),
+					headers: {
+						"Content-Type": "application/json"
+					}
+				};
+				fetch(endpoint, config)
+					.then(response => {
+						return response.json();
+					})
+					.then(json => {
+						getActions().listContacts("Tibisfly");
+					});
+			},
+			getContact(id) {
+				const endpoint = "https://assets.breatheco.de/apis/fake/contact/:id";
+				const config = {
+					method: "GET"
+				};
+
+				fetch(endpoint, config)
+					.then(response => {
+						return response.json();
+					})
+					.then(json => {
+						return json;
+					});
+			},
+			deleteContact(id) {
+				const endpoint = "https://assets.breatheco.de/apis/fake/contact/:id";
+				const config = {
+					method: "DELETE"
+				};
+
+				fetch(endpoint, config)
+					.then(response => {
+						return response.json();
+					})
+					.then(json => {
+						return json;
+					});
+			},
+			updateContact() {
+				const endpoint = "https://assets.breatheco.de/apis/fake/contact/:id";
+				const config = {
+					method: "PUT",
+					body: JSON.stringify({
+						full_name: data.name,
+						email: data.email,
+						agenda_slug: "Tibisfly",
+						address: data.address,
+						phone: data.phone
+					}),
+					headers: {
+						"Content-Type": "application/json"
+					}
+				};
+				fetch(endpoint, config)
+					.then(response => {
+						return response.json();
+					})
+					.then(json => {
+						getActions().listContacts("Tibisfly");
+					});
+			}
 		}
 	};
 };
-
+// evitar error de compatibilidad
 export default getState;
--- src/js/views/Contacts.js
+++ src/js/views/Contacts.js
@@ -1,6 +1,7 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useContext } from "react";
+import { Context } from "../store/appContext";
 import { Link } from "react-router-dom";
-
+import PropTypes from "prop-types";
 import { ContactCard } from "../component/ContactCard.js";
 import { Modal } from "../component/Modal";
 
@@ -8,6 +9,8 @@ export const Contacts = () => {
 	const [state, setState] = useState({
 		showModal: false
 	});
+
+	const { store, actions } = useContext(Context);
 
 	return (
 		<div className="container">
@@ -19,10 +22,10 @@ export const Contacts = () => {
 				</p>
 				<div id="contacts" className="panel-collapse collapse show" aria-expanded="true">
 					<ul className="list-group pull-down" id="contact-list">
-						<ContactCard onDelete={() => setState({ showModal: true })} />
-						<ContactCard />
-						<ContactCard />
-						<ContactCard />
+						{store.contacts.map((contact, key) => {
+							return <ContactCard data={contact} key={key} />;
+						})}
+						{/* <ContactCard onDelete={() => setState({ showModal: true })} /> */}
 					</ul>
 				</div>
 			</div>
